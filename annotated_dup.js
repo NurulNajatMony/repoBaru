3330c437c4c73a2c5e74577af5ddb76778c2e843
NurulNajatMony <nurulmony505@gmail.com>
2025-07-06 22:07:27 +0700

    Menambahkan data anotasi struktur dan kelas

---
+++ annotated_dup.js
@@ -0,0 +1,7 @@
+var annotated_dup =
+[
+    [ "node", "classnode.html", "classnode" ],
+    [ "Node", "class_node.html", null ],
+    [ "player", "structplayer.html", null ],
+    [ "Stack", "class_stack.html", "class_stack" ]
+];
