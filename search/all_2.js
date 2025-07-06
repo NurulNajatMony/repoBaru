89b29b6c7fc71d4212d5e32906bb9748104acee2
NurulNajatMony <nurulmony505@gmail.com>
2025-07-06 22:37:07 +0700

    Perbarui data pencarian untuk kelas dan atribut node

---
+++ search/all_2.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['next_0',['next',['../classnode.html#aad210fa7c160a49f6b9a3ffee592a2bc',1,'node']]],
+  ['node_1',['node',['../classnode.html',1,'']]],
+  ['node_2',['Node',['../class_node.html',1,'']]]
+];
