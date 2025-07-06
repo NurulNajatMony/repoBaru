1adedbc976220ecbb62f94c219454ec7c7c50f5a
NurulNajatMony <nurulmony505@gmail.com>
2025-07-06 22:12:19 +0700

    add member mapping for Stack class

---
+++ class_stack.js
@@ -0,0 +1,7 @@
+var class_stack =
+[
+    [ "isEmpty", "class_stack.html#acfd33dabc532e2706dea1699a4de2636", null ],
+    [ "peek", "class_stack.html#a97a88b0cbb0fc1c665f09ea4323c0234", null ],
+    [ "pop", "class_stack.html#a09e820f3c3531cf3f401af3b3ca5d56f", null ],
+    [ "push", "class_stack.html#aef99c2a68e11882730caec5add6d7480", null ]
+];
