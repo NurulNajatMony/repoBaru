f7ab42eac5ff5314eebb764c9dfce38cd0ce4864
NurulNajatMony <nurulmony505@gmail.com>
2025-07-06 22:38:16 +0700

    tambah fungsi peek

---
+++ search/all_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['peek_0',['peek',['../class_stack.html#a97a88b0cbb0fc1c665f09ea4323c0234',1,'Stack']]],
+  ['player_1',['player',['../structplayer.html',1,'']]],
+  ['pop_2',['pop',['../class_stack.html#a09e820f3c3531cf3f401af3b3ca5d56f',1,'Stack']]],
+  ['push_3',['push',['../class_stack.html#aef99c2a68e11882730caec5add6d7480',1,'Stack']]]
+];
